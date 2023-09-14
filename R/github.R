6cc2d09cdfb4a135b450b8ef9c50bfcaf91dc982
Luke W. Johnston <lwjohnst@gmail.com>
2023-09-14 18:01:37 +0200

    functions to add people to gh org

---
+++ R/github.R
@@ -0,0 +1,17 @@
+
+add_user_to_org <- function(username) {
+  checkmate::assert_character(username)
+  ghclass::org_invite("steno-aarhus", username)
+}
+
+add_user_to_common_docs_team <- function(username) {
+  checkmate::assert_character(username)
+  return(list(
+    ghclass::org_invite("steno-aarhus", username),
+    ghclass::team_invite("steno-aarhus", "common-docs-team")
+  ))
+}
+
+list_github_teams <- function() {
+  ghclass::org_teams("steno-aarhus")
+}
