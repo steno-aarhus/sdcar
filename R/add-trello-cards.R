45e181a0f4b9231ab3c05493723ca4995e83d21d
Luke W. Johnston <lwjohnst@gmail.com>
2023-11-13 22:34:24 +0100

    feat: functions to update trello board with cards

--- R/add-trello-cards.R
+++ R/add-trello-cards.R
@@ -1,62 +1,93 @@
-source(here::here("R/functions.R"))
-# Comments will be added over time.
-library(tidyverse)
-library(lubridate)
-library(calendar)
-library(trelloR)
-library(askpass)
+epi_board_url <- "https://trello.com/b/ipcYGXhC/epidemiology-group"
 
-stop("This prevents accidentally sourcing the whole script.")
-
-# Details about getting Key and OAuth Secret found here:
-# https://developer.atlassian.com/cloud/trello/guides/rest-api/authorization/
-trello_key <- askpass("Trello Key.")
-trello_oauth_secret <- askpass("Trello OAuth Secret.")
-
-token <- get_token(
+#' Authenticate with the Epi Group Trello board.
+#'
+#' Details about getting Key and OAuth Secret are found on this
+#' [website](https://developer.atlassian.com/cloud/trello/guides/rest-api/authorization/).
+#' @return A token used to connect and authenticate with Trello.
+#'
+tr_auth_epi <- function() {
+  trello_key <- askpass::askpass("Trello Key.")
+  trello_oauth_secret <- askpass::askpass("Trello OAuth Secret.")
+  trelloR::get_token(
     "steno-epi-board",
     key = trello_key,
     secret = trello_oauth_secret,
     scope = c("read", "write"),
     expiration = "1day",
     cache = FALSE
-)
+  )
+}
 
-board_url <- "https://trello.com/b/ipcYGXhC/epidemiology-group"
-upcoming_board_id <- get_board_lists(board_url, token = token) %>%
-    filter(str_detect(name, "Upcoming")) %>%
-    pull(id)
+#' Get the ID for a Trello board label.
+#'
+#' @param url The board URL.
+#' @param label The name of the board label you want the ID for.
+#' @param token The token obtained from [tr_auth_epi()].
+#'
+#' @return A character vector of the label ID.
+#'
+tr_get_board_label_id <- function(url = epi_board_url, label = "Meeting", token) {
+  trelloR::get_board_labels(board_url, token = token) %>%
+    dplyr::filter(name == label) %>%
+    dplyr::pull(id)
+}
 
-public_calendar_link <- "https://calendar.google.com/calendar/ical/086okoggkv7c4b0dcbbrj230s8%40group.calendar.google.com/public/basic.ics"
-upcoming_meetings <- read_ical(public_calendar_link)
-
-meetings_to_add <- upcoming_meetings %>%
-    transmute(year = year(DTSTART),
-              date = as.character(as_date(DTSTART))) %>%
-    filter(year == "2023")
-
-meeting_label <- get_board_labels(board_url, token = token) %>%
-    filter(name == "Meeting") %>%
-    pull(id)
-
-zoom_link <- askpass("Add Zoom link.")
-zoom_passcode <- askpass("Add Zoom passcode.")
-template_card_desc <- read_lines(here::here("R/card-template.md")) %>%
+#' Get the card template to use when making Trello cards.
+#'
+#' @return A character vector of the card template.
+#'
+tr_read_card_template <- function() {
+  zoom_link <- askpass::askpass("Add Zoom link.")
+  zoom_passcode <- askpass::askpass("Add Zoom passcode.")
+  template_card_desc <- readr::read_lines(fs::path_package("sdcar", "templates", "trello-meeting-card.md")) |>
     whisker::whisker.render(data = list(
-        passcode = zoom_passcode,
-        link = zoom_link
+      passcode = zoom_passcode,
+      link = zoom_link
     ))
+}
 
-meetings_to_add$date %>%
-    walk(
-        ~ add_card(
-            upcoming_board_id,
-            body = list(
-                name = .x,
-                desc = template_card_desc,
-                pos = "bottom",
-                idLabels = array(meeting_label)
-            ),
-            token = token
-        )
+#' Get the ID for a Trello board.
+#'
+#' @param name The name of the board you want the ID for.
+#' @inheritParams tr_get_board_label_id
+#'
+#' @return A character vector of the board ID.
+#'
+tr_get_board_id <- function(url = epi_board_url, name = "Upcoming", token) {
+  trelloR::get_board_lists(board_url, token = token) |>
+    dplyr::filter(stringr::str_detect(name, name)) |>
+    dplyr::pull(id)
+}
+
+#' Add upcoming meetings to Trello as cards.
+#'
+#' @param data Data that contains the month and day of the upcoming meetings.
+#' @inheritParams tr_get_board_label_id
+#'
+#' @return Used for the side effect of posting to Trello.
+#' @export
+#'
+update_epi_trello_meetings <- function(data, token) {
+  dates <- data |>
+    dplyr::mutate(dates = lubridate::ymd(glue::glue("{next_year()}-{month}-{day}"), tz = "Europe/Copenhagen")) |>
+    dplyr::pull(dates)
+
+  upcoming_board_id <- tr_get_board_id(token = token)
+  template_card <- tr_read_card_template()
+  label <- tr_get_board_label_id(token = token)
+
+  purrr::walk(
+    dates,
+    ~ trelloR::add_card(
+      upcoming_board_id,
+      body = list(
+        name = .x,
+        desc = template_card,
+        pos = "bottom",
+        idLabels = array(label)
+      ),
+      token = token
     )
+  )
+}
